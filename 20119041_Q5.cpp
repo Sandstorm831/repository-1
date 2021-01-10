52b82a6ab5714aef7575317b44596c0c8e30d3f3
Rohan <gargroham138@gmail.com>
2021-01-10 17:21:01 +0530

    filer

---
+++ 20119041_Q5.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"hello \n world \n";
+    cout<<"hello \t world \n";
+    cout<<"hello \v world \n";
+    cout<<"hello \\ world \n";
+    cout<<"hello \' world \n";
+    cout<<"hello \" world \n";
+    cout<<"\t A \n";
+    cout<<"B \t \t C \n";
+    cout<<"\t D \n";
+    return 0;
+
+}
